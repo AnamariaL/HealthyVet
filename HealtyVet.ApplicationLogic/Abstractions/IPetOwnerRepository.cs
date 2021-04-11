98c6c6eaf60f07033976762cc0d7c03ddc5e08a2
stan98bianca <bianca.stan9898@gmail.com>
2021-04-11 17:44:35 +0300

    add service view for pet owners to be viewed/ add feedback functionality (add delete)

---
+++ HealtyVet.ApplicationLogic/Abstractions/IPetOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using HealtyVet.ApplicationLogic.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealtyVet.ApplicationLogic.Abstractions
+{
+    public interface IPetOwnerRepository : IRepository<PetOwner>
+    {
+        PetOwner GetPetOwnerById(Guid petownerId);
+        
+        void Add(Feedback feedback);
+    }
+}
